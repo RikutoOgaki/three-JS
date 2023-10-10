9a42e586103f36145ecc03475300f73f362d0898
Rikuto <orikutobasuke@gmail.com>
2023-10-10 10:33:21 +0900

    sample01編集中

---
+++ src/pages/samples/01/index.tsx
@@ -0,0 +1,13 @@
+import * as THREE from 'three'
+import { useState, useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import style from '@/styles/samples/01/index.module.scss'
+
+export default function Sample01() {
+
+    return (
+        <>
+
+        </>
+    )
+}
---
+++ src/pages/samples/02/index.tsx
@@ -0,0 +1,5 @@
+export default function Sample02() {
+    return (
+        <></>
+    )
+}
